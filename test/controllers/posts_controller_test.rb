5c6a712302ee995bdf1b8879f3f3fdc61ea189fd
Carmine Moleti <carmine@moleti.it>
2014-08-27 14:50:29 +0200

    Used minitest/mock to test for correct interaction between PostsController#create and Redis server

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -44,4 +44,22 @@ class PostsControllerTest < ActionController::TestCase
     assert_response :unprocessable_entity
   end
 
+  test "post create when sucessful publish 'posts.create' to redis" do
+    post = Post.new(message: 'a brave new world', user_id: users(:dexter).id)
+    login_as(users(:dexter))
+    age_token(@user,@client_id)
+    request.headers.merge!(@auth_headers)
+
+    redis_mock = MiniTest::Mock.new
+
+    Redis.current = redis_mock
+    # We cannot be more specific about what is published to Redis
+    # since we lack of post's id,created_at,updated_at informations
+    redis_mock.expect(:publish,nil,['posts.create', String])
+
+    xhr :post, :create, format: :json, post: post.attributes
+    assert_response :ok
+
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'minitest/mock'
 require 'helpers/test_password'
 require 'helpers/auth'
 
